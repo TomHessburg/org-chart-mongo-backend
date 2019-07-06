44488b975dda5005e3f8ee1b84dff6222e081d3e
Thomas Hessburg <44884308+TomHessburg@users.noreply.github.com>
2019-07-06 14:13:10 -0400

    deps added and initial server setup complete

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const { json, urlencoded } = require("body-parser");
+const mongoose = require("mongoose");
+
+// import routes
+
+// server instantiation and middleware
+const server = express();
+server.use(cors());
+server.use(json());
+server.use(urlencoded({ extended: true }));
+server.use(morgan("dev"));
+
+const connect = () => {
+  return mongoose.connect("mongodb://localhost:27017/org-chart", {
+    useNewUrlParser: true
+  });
+};
+
+const port = process.env.PORT || 5000;
+connect()
+  .then(conn => {
+    server.listen(port, () => {
+      console.log(`Server listning on port: ${port}`);
+    });
+  })
+  .catch(err => {
+    console.log("error connecting to your server", err);
+  });
